43d91af0bd1577579a5e4c9f5a534ea8a2a9a53b
pnauts <dark.nobunaga@gmail.com>
2016-04-23 18:19:54 +0200

    Solo qudan relevance feedback

--- P3/src/main/java/RI/P3/CACMEval.java
+++ P3/src/main/java/RI/P3/CACMEval.java
@@ -7,10 +7,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -19,6 +21,7 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.flexible.standard.QueryParserUtil;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
@@ -30,6 +33,116 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 public class CACMEval {
+
+	private static class queryRelevance {
+
+		private final int queryId;
+		private final List<String> docs;
+
+		public queryRelevance(int queryId, List<String> docs) {
+
+			this.queryId = queryId;
+			this.docs = docs;
+		}
+
+		public int getQueryId() {
+			return queryId;
+		}
+
+		public List<String> getDocs() {
+			return docs;
+		}
+
+	}
+
+	private static class docElement implements Comparable<docElement> {
+
+		private final float doc_score;
+		private final String i_docID;
+		private final String t_title;
+		private final String w_abstract;
+		private final String b_date;
+		private final String a_authors;
+		private final String k_keywords;
+		private final String n_entrydate;
+
+		public docElement(float doc_score, String i_docID, String t_title, String w_abstract, String b_date,
+				String a_authors, String k_keywords, String n_entrydate) {
+			this.doc_score = doc_score;
+			this.i_docID = i_docID;
+			this.t_title = t_title;
+			this.w_abstract = w_abstract;
+			this.b_date = b_date;
+			this.a_authors = a_authors;
+			this.k_keywords = k_keywords;
+			this.n_entrydate = n_entrydate;
+		}
+
+		public int compareTo(docElement o) {
+			return Double.compare(o.doc_score, this.doc_score);
+		}
+
+		public float getDoc_score() {
+			return doc_score;
+		}
+
+		public String getI_docID() {
+			return i_docID;
+		}
+
+		public String getT_title() {
+			return t_title;
+		}
+
+		public String getW_abstract() {
+			return w_abstract;
+		}
+
+		public String getB_date() {
+			return b_date;
+		}
+
+		public String getA_authors() {
+			return a_authors;
+		}
+
+		public String getK_keywords() {
+			return k_keywords;
+		}
+
+		public String getN_entrydate() {
+			return n_entrydate;
+		}
+
+		public void showFields(List<String> fields) {
+
+			if (fields.contains("docid"))
+				System.out.println("	docid: " + this.getI_docID());
+
+			if (fields.contains("title"))
+				System.out.println("	title: " + this.getT_title());
+
+			if (fields.contains("abstract"))
+				System.out.println("	abstract: " + this.getW_abstract());
+
+			if (fields.contains("date"))
+				System.out.println("	date: " + this.getB_date());
+
+			if (fields.contains("authors"))
+				System.out.println("	authors: " + this.getA_authors());
+
+			if (fields.contains("keywords"))
+				System.out.println("	keywords: " + this.getK_keywords());
+
+			if (fields.contains("entrydate"))
+				System.out.println("	entrydate: " + this.getN_entrydate());
+
+			System.out.println("	score: " + this.getDoc_score());
+
+			System.out.println();
+		}
+
+	}
 
 	public static class QueryData {
 
@@ -97,16 +210,14 @@ public class CACMEval {
 			return document;
 		}
 
-		private static String extract(String elt, String text,
-				boolean allowEmpty) {
+		private static String extract(String elt, String text, boolean allowEmpty) {
 
 			String startElt = "." + elt;
 			int startEltIndex = text.indexOf(startElt);
 			if (startEltIndex < 0) {
 				if (allowEmpty)
 					return "";
-				throw new IllegalArgumentException("no start, elt=" + elt
-						+ " text=" + text);
+				throw new IllegalArgumentException("no start, elt=" + elt + " text=" + text);
 			}
 			int start = startEltIndex + startElt.length();
 			String endElt = "\n.";
@@ -146,46 +257,63 @@ public class CACMEval {
 
 			else if ("-queries".equals(args[i])) {
 				i++;
-
 				if ("all".equals(args[i]))
 					continue;
 
 				if (args[i].indexOf('-') != -1) {
 					String queryNumbers = args[i].replace(" ", "");
-					minQuery = Integer.parseInt(queryNumbers.substring(0,
-							queryNumbers.indexOf('-')));
-					maxQuery = Integer.parseInt(queryNumbers.substring(
-							queryNumbers.indexOf('-') + 1,
-							queryNumbers.length() - 1));
+					minQuery = Integer.parseInt(queryNumbers.substring(0, queryNumbers.indexOf('-')));
+					maxQuery = Integer
+							.parseInt(queryNumbers.substring(queryNumbers.indexOf('-') + 1, queryNumbers.length()));
 				}
 
-				else
+				else {
 					minQuery = Integer.parseInt(args[i]);
+					maxQuery = Integer.parseInt(args[i]);
+				}
 
 			}
 
 			else if ("-fieldsproc".equals(args[i])) {
-				i++;
-				while (i < args.length && !args[i].startsWith("-")) {
-					
-					if(args[i].toLowerCase().equals("w"))
-						args[i] = "abstract";
-					
-					else if(args[i].toLowerCase().equals("t"))
-						args[i] = "title";
-					
-					else if(args[i].toLowerCase().equals("k"))
-						args[i] = "keywords";
-					
-					fieldsProc.add(args[i]);
+
+				while (i < args.length && !args[i + 1].startsWith("-")) {
+
+					if (args[i + 1].toLowerCase().equals("w"))
+						args[i + 1] = "abstract";
+
+					else if (args[i + 1].toLowerCase().equals("t"))
+						args[i + 1] = "title";
+
+					else if (args[i + 1].toLowerCase().equals("k"))
+						args[i + 1] = "keywords";
+
+					fieldsProc.add(args[i + 1]);
 					i++;
 				}
 			}
 
 			else if ("-fieldsvisual".equals(args[i])) {
-				i++;
-				while (i < args.length && !args[i].startsWith("-")) {
-					fieldsVisual.add(args[i]);
+
+				while (i < args.length && !args[i + 1].startsWith("-")) {
+
+					String field = null;
+
+					if (args[i].toLowerCase().equals("i"))
+						field = "docid";
+					if (args[i].toLowerCase().equals("t"))
+						field = "title";
+					if (args[i].toLowerCase().equals("w"))
+						field = "abstract";
+					if (args[i].toLowerCase().equals("b"))
+						field = "date";
+					if (args[i].toLowerCase().equals("a"))
+						field = "authors";
+					if (args[i].toLowerCase().equals("k"))
+						field = "keywords";
+					if (args[i].toLowerCase().equals("n"))
+						field = "entrydate";
+
+					fieldsVisual.add(field);
 					i++;
 				}
 			}
@@ -213,13 +341,48 @@ public class CACMEval {
 
 		String queryDir = System.getProperty("user.dir");
 		queryDir = queryDir + "\\index\\query.text";
-		queryMap = getQueries(Paths.get(queryDir));
-		
-		processQuery(indexPath, "What articles exist which deal with TSS (Time Sharing System), an operating system for IBM computers?",fieldsProc);
+
+		String relevanceDir = System.getProperty("user.dir");
+		relevanceDir = relevanceDir + "\\index\\qrels.text";
+
+		List<queryRelevance> relevances = getRelevances(Paths.get(relevanceDir));
+
+		queryMap = getQueries(Paths.get(queryDir), minQuery, maxQuery);
+		doSearch(indexPath, queryMap, topLimit, fieldsProc, fieldsVisual, relevances, cutLimit);
 	}
 
-	public static Map<Integer, QueryData> getQueries(Path queryFile) {
-		Map<Integer, QueryData> map = new HashMap<Integer, QueryData>();
+	public static List<queryRelevance> getRelevances(Path file) {
+
+		List<queryRelevance> relevances = new ArrayList<queryRelevance>();
+
+		try {
+			byte[] encoded = Files.readAllBytes(file);
+			String text = new String(encoded, StandardCharsets.UTF_8);
+			String[] lines = text.split("\n");
+			int queriesNumber = Integer.parseInt(lines[lines.length - 1].substring(0, 2));
+			int index = 0;
+
+			for (int i = 0; i <= queriesNumber; i++) {
+
+				List<String> rls = new ArrayList<String>();
+
+				while (index < lines.length && i == Integer.parseInt((lines[index].substring(0, 2)))) {
+					rls.add(lines[index].substring(3, 7));
+					index++;
+				}
+
+				queryRelevance qr = new queryRelevance(i, rls);
+				relevances.add(qr);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return relevances;
+	}
+
+	public static Map<Integer, QueryData> getQueries(Path queryFile, int minQuery, int maxQuery) {
+		Map<Integer, QueryData> map = new TreeMap<Integer, QueryData>();
 
 		try {
 
@@ -234,11 +397,13 @@ public class CACMEval {
 			for (List<String> qData : dataList) {
 
 				Integer queryId = Integer.parseInt(qData.get(0));
-				String query = qData.get(1);
-				String authors = qData.get(2);
-				String entry = qData.get(3);
+				if (minQuery == -1 || (queryId >= minQuery && queryId <= maxQuery)) {
 
-				map.put(queryId, new QueryData(query, authors, entry));
+					String query = qData.get(1);
+					String authors = qData.get(2);
+					String entry = qData.get(3);
+					map.put(queryId, new QueryData(query, authors, entry));
+				}
 			}
 
 		} catch (IOException e) {
@@ -248,48 +413,75 @@ public class CACMEval {
 		return map;
 	}
 
-	public static void processQuery(String indexPath, String query,
-			List<String> fields) {
+	public static float processQuery(String indexPath, String query, List<String> procFields, List<String> showFields,
+			int top, queryRelevance qr, int cut) {
 
 		IndexReader reader;
+		List<docElement> docs = new ArrayList<docElement>();
+		float sumP = 0;
 
 		try {
 
-			reader = DirectoryReader
-					.open(FSDirectory.open(new File(indexPath)));
+			reader = DirectoryReader.open(FSDirectory.open(new File(indexPath)));
 			IndexSearcher searcher = new IndexSearcher(reader);
 			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
 
-			String[] arrayQuery = new String[fields.size()];
-			String[] arrayField = new String[fields.size()];
-			Occur[] arrayClauses = new Occur[fields.size()];
+			String[] arrayQuery = new String[procFields.size()];
+			String[] arrayField = new String[procFields.size()];
+			Occur[] arrayClauses = new Occur[procFields.size()];
 
-			for (int i = 0; i < fields.size(); i++) {
+			query = QueryParserUtil.escape(query);
+
+			for (int i = 0; i < procFields.size(); i++) {
 
 				arrayQuery[i] = query;
-				arrayField[i] = fields.get(i);
+				arrayField[i] = procFields.get(i);
 				arrayClauses[i] = BooleanClause.Occur.SHOULD;
 			}
 
-			MultiFieldQueryParser parser = new MultiFieldQueryParser(
-					Version.LUCENE_40, arrayField, analyzer);
-
-			BooleanQuery booleanQuery = (BooleanQuery) MultiFieldQueryParser
-					.parse(arrayQuery, arrayField, arrayClauses, analyzer);
+			BooleanQuery booleanQuery = (BooleanQuery) MultiFieldQueryParser.parse(arrayQuery, arrayField, arrayClauses,
+					analyzer);
 
 			TotalHitCountCollector collector = new TotalHitCountCollector();
 			searcher.search(booleanQuery, collector);
-			TopDocs results = searcher.search(booleanQuery,
-					collector.getTotalHits());
+			TopDocs results = searcher.search(booleanQuery, collector.getTotalHits());
 			ScoreDoc[] hits = results.scoreDocs;
-			
-			for (int i = 0; i < results.totalHits; i++) {
-				Document doc = searcher.doc(hits[i].doc);
 
-				System.out.println("DocID: " + doc.get("docid"));
-				System.out.println("Title: " + doc.get("title"));
-				System.out.println("Score: " + hits[i].score);
-				System.out.println();
+			for (int i = 0; i < collector.getTotalHits(); i++) {
+				Document doc = searcher.doc(hits[i].doc);
+				Float score = hits[i].score;
+				docElement de = new docElement(score, doc.get("docid"), doc.get("title"), doc.get("abstract"),
+						doc.get("date"), doc.get("authors"), doc.get("keywords"), doc.get("entrydate"));
+
+				docs.add(de);
+			}
+
+			Collections.sort(docs);
+			System.out.println("P@10: " + calculatePK(10, qr, docs));
+			System.out.println("P@20: " + calculatePK(20, qr, docs));
+			System.out.println("Recall@10: " + calculateRK(10, qr, docs));
+			System.out.println("Recall@20: " + calculateRK(20, qr, docs));
+
+			System.out.println();
+
+			for (int i = 0; i < cut; i++) {
+				sumP += calculatePK(i + 1, qr, docs);
+			}
+
+			for (int i = 0; i < top; i++) {
+				docElement de = docs.get(i);
+
+				if (qr != null && qr.getDocs().contains(de.getI_docID())) {
+					System.out.println("	***THE FOLLOWING DOC IS RELEVANT***");
+					System.out.println("	Top " + (i + 1) + " doc");
+				}
+
+				else
+					System.out.println("	Top " + (i + 1) + " doc");
+
+				System.out.println("	Avg. Precision: " + calculatePK(i + 1, qr, docs));
+
+				de.showFields(showFields);
 			}
 
 		} catch (IOException e) {
@@ -297,5 +489,64 @@ public class CACMEval {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+
+		return sumP / (float) cut;
 	}
+
+	private static float calculateRK(int k, queryRelevance qr, List<docElement> docs) {
+
+		int relevants = 0;
+
+		for (int i = 0; i < k; i++) {
+
+			if (qr.getDocs().contains(docs.get(i).getI_docID()))
+				relevants++;
+		}
+
+		float pk = (relevants / (float) qr.getDocs().size());
+		return pk;
+
+	}
+
+	private static float calculatePK(float k, queryRelevance qr, List<docElement> docs) {
+
+		int relevants = 0;
+
+		for (int i = 0; i < k; i++) {
+
+			if (qr.getDocs().contains(docs.get(i).getI_docID()))
+				relevants++;
+		}
+		float rk = (relevants / k);
+		return rk;
+	}
+
+	public static void doSearch(String indexPath, Map<Integer, QueryData> mapQueries, int top, List<String> procFields,
+			List<String> showFields, List<queryRelevance> relevances, int cut) {
+
+		float map = 0;
+
+		System.err.println("Showing requested search info...\n");
+
+		for (Map.Entry<Integer, QueryData> entry : mapQueries.entrySet()) {
+			int queryNumber = entry.getKey();
+			String query = entry.getValue().getQuery();
+			queryRelevance qr = null;
+
+			System.out.println("QueryID :" + queryNumber);
+			System.out.println("Query Body :" + query);
+			System.out.println("Showing top " + top + " docs:");
+
+			if (queryNumber <= (relevances.size() + 1))
+				qr = relevances.get(queryNumber);
+
+			map += processQuery(indexPath, query, procFields, showFields, top, qr, cut);
+			System.out.println();
+
+		}
+
+		System.err.println("Mean average precision: " + map / mapQueries.size());
+
+	}
+
 }
